98745da1532dd926dce99e4409a0ebfbf3e47f99
Dacidon <thremormagia@gmail.com>
2024-01-14 18:22:39 +0300

    Добавлены пояснения к задачам

--- task2.php
+++ task2.php
@@ -1,7 +1,9 @@
 <?php
-define('TOTAL_DRAW_VALUE', 80);
-define('PENCIL_DRAW_VALUE', 40);
-define('MARKER_DRAW_VALUE', 23);
+// ДЗ 1 Задание 2
+// Решить задачу и вывести её условие
+const TOTAL_DRAW_VALUE = 80;
+const PENCIL_DRAW_VALUE = 40;
+const MARKER_DRAW_VALUE = 23;
 
 echo "На школьной выставке " . TOTAL_DRAW_VALUE . " рисунков. " . MARKER_DRAW_VALUE . " из них выполнены <br/>фломастерами, " . PENCIL_DRAW_VALUE . " карандашами, а остальные — красками. <br/>Сколько рисунков, выполненные красками, на школьной выставке? <br/><br/>";
 echo "Для решения задачи потребуется вычесть из общего количества рисунков (" . TOTAL_DRAW_VALUE . ") <br/>сумму количества рисунков, выполненных при помощи карандашей (" . PENCIL_DRAW_VALUE . ") <br/>и рисунков, выполненных при помощи фломастеров (" . MARKER_DRAW_VALUE . "). <br/>";
